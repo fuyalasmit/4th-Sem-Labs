cec3f0f271d4baf251a1c233c5d556e45624dcd9
Asmit Phuyal <asmitphuyal4040@gmail.com>
2024-12-16 21:02:38 +0545

    lab 2

--- NM lab/Lab 2/bisectionMethod.cpp
+++ NM lab/Lab 2/bisectionMethod.cpp
@@ -5,22 +5,23 @@ using namespace std;
 
 double f(double x)
 {
-    // return exp(x)-3*x;
+    return exp(x) - 3 * x;
     // return x*x + tan(x) + exp(x);
+    // return 1 / (x * x * x);
+    // return 1/(pow(x,3)) + sin(x);
 }
 
 int main()
 {
-
-    double a, b;
+    double a, b, c;
     int d;
-    cout << "Enter the value of a and b: ";
+    cout << "Enter the value of a and b(starting boundary): ";
     cin >> a >> b;
-    cout << "Enter the number for decimal tolerance: ";
+    cout << "Enter the decimal place for tolerance: ";
     cin >> d;
     double tolerance = 0.5 * (pow(10, -d));
-    int n = ceil(log(abs(b - a) - log(tolerance)) / log(2));
-
+    // int n = ceil(log(abs(b - a) - log(tolerance)) / log(2));
+    int n = ceil((log(abs(b - a)) - log(tolerance)) / log(2));
     if (f(a) * f(b) > 0)
     {
         cout << "Error, no value between these two inputs";
@@ -32,7 +33,7 @@ int main()
         {
             cout << a << endl;
         }
-        if (f(b) == 0)
+        else
         {
             cout << b << endl;
         }
@@ -40,10 +41,9 @@ int main()
     else
     {
         int count = 0;
-        double c;
         do
         {
-            c = (a + b) / 2;
+            c = (a + b) / 2.0;
             if (f(c) * f(a) < 0)
             {
                 b = c;
@@ -52,9 +52,10 @@ int main()
             {
                 a = c;
             }
+            count++;
             if (count > 2 * n)
             {
-                cout << "Discontinuous";
+                cout << "Discontinuous, can't find the value of this function." << endl;
                 return 1;
             }
         } while (abs(f(c)) >= tolerance);
